72ced182c8f9abddb1db2c98ca20b2526696912d
Ljubisav Curcic <ljubisav.curcic@gmail.com>
2018-03-12 02:02:11 +0100

    setting angular code for work with tabs and router

--- helloworld.js
+++ helloworld.js
@@ -1,6 +1,6 @@
 var myApp = angular.module('helloworld', ['ui.router', 'ui.bootstrap']);
 
-myApp.config(function($stateProvider) {
+myApp.config(function($stateProvider, $urlRouterProvider) {
     $stateProvider
     .state('home', {
         url: '/',
@@ -8,6 +8,61 @@ myApp.config(function($stateProvider) {
     })
     .state('tabs', {
         url: '/tabs',
-        templateUrl: 'tabs.html'
-    }); 
+        templateUrl: 'tabs.html',
+        controller:'TabsDemoCtrl'
+    })
+    .state('tabs.tab1', {
+        url: '/tab1',
+        templateUrl: 'tabs.html',
+        controller:'TabsDemoCtrl'
+    })
+    .state('tabs.tab2', {
+        url: '/tab2',
+        templateUrl: 'tabs.html',
+        controller:'TabsDemoCtrl'
+    })    
+    ;
+
+    $urlRouterProvider.otherwise('/');
+});
+
+
+myApp.controller('TabsDemoCtrl', function ($scope, $window, $state) {
+    $scope.$state = $state;
+    $scope.activeTabIndex = 1;
+    console.log('entered TabsDemoCtrl with state: ' + $state.current.name);
+
+    $scope.tabs = [
+      { title:'Dynamic Title 1', content:'Dynamic content 1' },
+      { title:'Dynamic Title 2', content:'Dynamic content 2', disabled: true }
+    ];
+  
+    $scope.alertMe = function() {
+      setTimeout(function() {
+        $window.alert('You\'ve selected the alert tab!');
+      });
+    };
+  
+    $scope.model = {
+      name: 'Tabs'
+    };
+  });
+
+
+myApp.controller('tabCtrl', function($scope, $state) {
+    console.log($state.current.name);
+    //debugger;
+    $scope.$state = $state;
+});
+
+myApp.controller('tab1Ctrl', function($scope, $state) {
+    console.log($state.current.name);
+    //debugger;
+    $scope.$state = $state;
+});
+
+myApp.controller('tab2Ctrl', function($scope, $state) {
+    console.log($state.current.name);
+    //debugger;
+    $scope.$state = $state;
 });
